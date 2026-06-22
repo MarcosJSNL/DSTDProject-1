60ba6d6cca9c22567bf06fc9d0cabbfa243b73fd
MarcosJSNL <cl72910@gmail.com>
2026-06-22 10:26:12 -0400

    Fix AnimatedHeading: prevent words from splitting mid-word by wrapping words in nowrap spans

--- src/components/AnimatedHeading.tsx
+++ src/components/AnimatedHeading.tsx
@@ -28,29 +28,52 @@ export default function AnimatedHeading({
 
   return (
     <h1 className={className} style={style}>
-      {lines.map((line, lineIndex) => (
-        <span key={lineIndex} className="block">
-          {line.split('').map((char, charIndex) => {
-            const delay =
-              lineIndex * line.length * charDelay + charIndex * charDelay
-            const displayChar = char === ' ' ? '\u00A0' : char
-            return (
-              <span
-                key={`${lineIndex}-${charIndex}`}
-                className="inline-block"
-                style={{
-                  opacity: triggered ? 1 : 0,
-                  transform: triggered ? 'translateX(0)' : 'translateX(-18px)',
-                  transition: `opacity ${transitionDuration}ms ease, transform ${transitionDuration}ms ease`,
-                  transitionDelay: `${delay}ms`,
-                }}
-              >
-                {displayChar}
-              </span>
-            )
-          })}
-        </span>
-      ))}
+      {lines.map((line, lineIndex) => {
+        const words = line.split(' ')
+        let charCounter = 0
+        return (
+          <span key={lineIndex} className="block">
+            {words.map((word, wordIndex) => {
+              const wordSpan = (
+                <span
+                  key={`word-${lineIndex}-${wordIndex}`}
+                  className="inline-block whitespace-nowrap"
+                >
+                  {word.split('').map((char, charIndex) => {
+                    const delay =
+                      lineIndex * line.length * charDelay +
+                      charCounter * charDelay
+                    charCounter += 1
+                    return (
+                      <span
+                        key={`${lineIndex}-${wordIndex}-${charIndex}`}
+                        className="inline-block"
+                        style={{
+                          opacity: triggered ? 1 : 0,
+                          transform: triggered
+                            ? 'translateX(0)'
+                            : 'translateX(-18px)',
+                          transition: `opacity ${transitionDuration}ms ease, transform ${transitionDuration}ms ease`,
+                          transitionDelay: `${delay}ms`,
+                        }}
+                      >
+                        {char}
+                      </span>
+                    )
+                  })}
+                </span>
+              )
+              charCounter += 1
+              return (
+                <span key={`wrap-${lineIndex}-${wordIndex}`}>
+                  {wordSpan}
+                  {wordIndex < words.length - 1 ? ' ' : null}
+                </span>
+              )
+            })}
+          </span>
+        )
+      })}
     </h1>
   )
 }
